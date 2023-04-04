cf59b65e909c1950e92ddf3ce617b382685c2e4e
kundan <goswamikundan123@gmail.com>
2023-04-04 12:24:48 +0530

    Added files which connect to the Database to add an Employee details and retrieve it for employee Payroll Services.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace EmployeePayrollDB
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll !");
+            EmpolyeeRepo employeeRepo = new EmpolyeeRepo();
+
+            EmployeeModel employeeModel = new EmployeeModel();
+
+            employeeModel.EmployeeName = "Kundan Kumar";
+            employeeModel.PhoneNumber = "123456789";
+            employeeModel.Address = "Motihari";
+            employeeModel.Department = "software";
+            employeeModel.Gender = 'M';
+            employeeModel.BasePay = 220000.00;
+            employeeModel.Deductions = 100.00;
+            employeeModel.TaxablePay = 250.00;
+            employeeModel.Tax = 300.00;
+            employeeModel.NetPay = 21000.00;
+            employeeModel.City = "Motihari";
+            employeeModel.Country = "India";
+
+            //employeeRepo.AddEmployee(employeeModel);
+
+            employeeRepo.GetAllEmployee();
+
+
+
+        }
+    }
+}
